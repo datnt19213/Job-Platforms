3653bd8faa6144c6698aee821679811cad83b3ab
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-05-28 18:07:20 +0700

    fix: Adjust component structure by moving GridLayout inside ScrollAnimator

--- src/features/candidate-detail/index.tsx
+++ src/features/candidate-detail/index.tsx
@@ -14,13 +14,12 @@ interface CandidateDetailsProps {
 export const CandidateDetails: React.FC<CandidateDetailsProps> = ({id}) => {
 	return (
 		<BodyContainer className="w-full !max-w-[970px] 1320:!max-w-[1320px] mx-auto pt-[60px] pb-[90px]">
-			<GridLayout className="!grid-cols-1 990:!grid-cols-[1fr_323px] 1200:!grid-cols-[1fr_440px] w-full gap-0">
-				<ScrollAnimator isZoom={false} fade slide="up" distancing={20}>
+			<ScrollAnimator isZoom={false} fade slide="up" distancing={20}>
+				<GridLayout className="!grid-cols-1 990:!grid-cols-[1fr_323px] 1200:!grid-cols-[1fr_440px] w-full gap-0">
 					<LeftDetail />
-
 					<RightDetail />
-				</ScrollAnimator>
-			</GridLayout>
+				</GridLayout>
+			</ScrollAnimator>
 		</BodyContainer>
 	);
 };
